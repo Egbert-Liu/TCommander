eb1cd9c17210d61cbd989f063d01c4cd3f0b5daf
egbert <1255987154@qq.com>
2026-06-06 13:03:20 +0800

    fix: update config files to CommonJS and remove deprecated Terminal component

--- src/renderer/src/components/Terminal.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect, useRef } from 'react'
-import { Terminal as XTerm } from 'xterm'
-import { FitAddon } from 'xterm-addon-fit'
-import 'xterm/css/xterm.css'
-
-interface TerminalProps {
-  sessionId: string
-  onData: (data: string) => void
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/renderer/src/**/*.{js,ts,jsx,tsx}",
